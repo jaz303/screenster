9e42cd6f9818ec81e42254bf3fc3f66f2fc75fdf
Jason <jason@ubuntu.(none)>
2012-12-13 08:32:16 -0800

    destroy screen handler

--- src/commands/builtin/cmd_destroy_screen.c
+++ src/commands/builtin/cmd_destroy_screen.c
@@ -1,1 +1,18 @@
-/* stub handler */
+if (cmd_len != 1) {
+	// TODO: handle error
+}
+
+char screen_id = READ_BYTE();
+
+printf("destroy screen requested (id=%d)\n", screen_id);
+
+if (screen_id < 1 || screen_id > MAX_SCREENS || screens[screen_id] == 0) {
+	printf("invalid screen id\n", screen_id);
+	// TODO: handle error
+	return;
+}
+
+al_destroy_display(screens[screen_id]);
+screens[screen_id] = 0;
+
+// TODO: reply
