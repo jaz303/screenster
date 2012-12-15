91922f10f99a45b5f1a36f82a1530196011d11d4
Jason Frame <jason@onehackoranother.com>
2012-12-15 15:22:51 -0800

    clear set colors to macro

--- src/commands/builtin/cmd_set_fill_color.c
+++ src/commands/builtin/cmd_set_fill_color.c
@@ -1,6 +1,1 @@
-draw_state.fill_color = al_map_rgba(
-	(argb >> 16) & 0xFF,
-	(argb >>  8) & 0xFF,
-	(argb >>  0) & 0xFF,
-	(argb >> 24) & 0xFF
-	);
+draw_state.fill_color = COLOR_TO_ALLEGRO(argb);
--- src/commands/builtin/cmd_set_pen_color.c
+++ src/commands/builtin/cmd_set_pen_color.c
@@ -1,6 +1,1 @@
-draw_state.pen_color = al_map_rgba(
-	(argb >> 16) & 0xFF,
-	(argb >>  8) & 0xFF,
-	(argb >>  0) & 0xFF,
-	(argb >> 24) & 0xFF
-	);
+draw_state.pen_color = COLOR_TO_ALLEGRO(argb);
