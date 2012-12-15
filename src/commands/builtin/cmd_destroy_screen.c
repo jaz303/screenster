74971b6e75e05080b9dcfb0d8c96262f0e2f66cc
Jason Frame <jason@onehackoranother.com>
2012-12-15 09:06:21 -0800

    setting active screen updates allegro target

--- src/commands/builtin/cmd_destroy_screen.c
+++ src/commands/builtin/cmd_destroy_screen.c
@@ -6,11 +6,12 @@ if (screen_id < 1 || screen_id > MAX_SCREENS || screens[screen_id] == 0) {
 	return;
 }
 
-al_destroy_display(screens[screen_id]);
-screens[screen_id] = 0;
-
 if (active_screen == screen_id) {
 	active_screen = 0;
+	al_set_target_bitmap(NULL);
 }
+
+al_destroy_display(screens[screen_id]);
+screens[screen_id] = NULL;
 
 // TODO: reply
--- src/commands/builtin/cmd_set_active_screen.c
+++ src/commands/builtin/cmd_set_active_screen.c
@@ -4,5 +4,6 @@ if (screen_id < 1 || screen_id > MAX_SCREENS || screens[screen_id] == 0) {
 }
 
 active_screen = screen_id;
+al_set_target_backbuffer(screens[active_screen]);
 
 // TODO: reply
