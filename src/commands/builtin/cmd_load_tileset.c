d838ebe50d2ae9f11ab6c35a5fbdd8924624865e
Jason Frame <jason@onehackoranother.com>
2013-01-15 16:36:32 +0000

    debug message in load tileset

--- src/commands/builtin/cmd_load_tileset.c
+++ src/commands/builtin/cmd_load_tileset.c
@@ -1,5 +1,6 @@
 obj_id_t tileset_id = load_tileset_from_file(filename, tile_width, tile_height, mask_color);
 if (OBJ_SUCCESS(tileset_id)) {
+    printf("tileset loaded, id = %d\n", tileset_id);
     // TODO: success
 } else {
     // TODO: error
